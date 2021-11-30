8450b1238fd1797bd29815612bfeecda2506596e
Raphael THIBAUT <thibaut.raphael@protonmail.com>
2021-11-30 10:53:14 +0100

    Je sais pas pk les fichiers ont changés

---
+++ 3 - .forEach Function/index.js
@@ -0,0 +1,42 @@
+function CreateSuspectObjects(name) {
+    return {
+        name: name,
+        color: name.split(' ')[1],
+        speak: function() {
+            console.log('My name is ' + name);
+        }
+    };
+};
+
+var suspects = {
+    "Miss Scarlet": "Miss Scarlette",
+    "Colonel Mustard": "Colonel Mustard",
+    "Mr. White": "Mr. White"    
+};
+
+// 'Miss Scarlet', 'Colonel Mustard', 'Mr. White'
+
+var missScarlet = CreateSuspectObjects(suspects['Miss Scarlet']);
+
+missScarlet.speak();
+
+// Hydratation
+const listSuspects = [];
+
+for(property in suspects) {
+    listSuspects.push(CreateSuspectObjects(suspects[property]));
+}
+
+function logger(value) {
+    console.log(value);
+}
+
+// listSuspects.forEach(e => console.log(e));
+
+listSuspects.forEach(e => {
+    for(property in e) { 
+        logger(listSuspects[0][property]);
+        console.log(`${property}: ${listSuspects[0][property]}``);
+    }}
+);
+    
---
+++ otherTrain/index.js
@@ -0,0 +1,20 @@
+const person = [{
+    "id": "JeanMiDu13",
+    "version": 1,
+    "first name": "Jean",
+    "last name": "Luc",
+    "favorites": {
+        "restaurants": ["Le comptoir", "Tacos"],
+        "dishes": ["Fish and Ships", "Kebab", "pate en sauce"]
+    }
+},
+{
+    "id": "Bernard5962",
+    "version": 1,
+    "first name": "Florian",
+    "last name": "Bernard",
+    "favorites": {
+        "restaurants": ["Dominos", "Tacos"],
+        "dishes": ["Pizza", "Kebab", "Salade"]
+    } 
+}]
