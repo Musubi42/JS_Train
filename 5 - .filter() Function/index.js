76c8821746130b8dcea8ecd6624954a360810e50
Raphael THIBAUT <thibaut.raphael@protonmail.com>
2021-12-03 20:14:05 +0100

    Filter function

---
+++ 5 - .filter() Function/index.js
@@ -0,0 +1,83 @@
+const videoData = [
+  {
+    name: "Miss Scarlet",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Mrs. White",
+    present: false,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Reverend Green",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Rusty",
+    present: false,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Colonel Mustard",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Professor Plum",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+];
+
+var present = [];
+
+videoData.forEach((e) => {
+  if (e.present === true) {
+    return present.push(e);
+  } else {
+  }
+});
