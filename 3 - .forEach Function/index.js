057ed78c03133ec1116102c26552a798a3d8c760
Raphael THIBAUT <thibaut.raphael@protonmail.com>
2021-12-03 20:13:15 +0100

    For Each

--- 3 - .forEach Function/index.js
+++ 3 - .forEach Function/index.js
@@ -1,42 +1,49 @@
+const underscore = require("underscore");
+
 function CreateSuspectObjects(name) {
-    return {
-        name: name,
-        color: name.split(' ')[1],
-        speak: function() {
-            console.log('My name is ' + name);
-        }
-    };
-};
+  return {
+    name: name,
+    color: name.split(" ")[1],
+    speak: function () {
+      console.log("My name is " + name);
+    },
+  };
+}
 
 var suspects = {
-    "Miss Scarlet": "Miss Scarlette",
-    "Colonel Mustard": "Colonel Mustard",
-    "Mr. White": "Mr. White"    
+  "Miss Scarlet": "Miss Scarlette",
+  "Colonel Mustard": "Colonel Mustard",
+  "Mr. White": "Mr. White",
 };
 
 // 'Miss Scarlet', 'Colonel Mustard', 'Mr. White'
 
-var missScarlet = CreateSuspectObjects(suspects['Miss Scarlet']);
+//var missScarlet = CreateSuspectObjects(suspects["Miss Scarlet"]);
 
 missScarlet.speak();
 
 // Hydratation
 const listSuspects = [];
 
-for(property in suspects) {
-    listSuspects.push(CreateSuspectObjects(suspects[property]));
+for (property in suspects) {
+  listSuspects.push(CreateSuspectObjects(suspects[property]));
 }
 
 function logger(value) {
-    console.log(value);
+  console.log(value);
 }
 
 // listSuspects.forEach(e => console.log(e));
 
-listSuspects.forEach(e => {
-    for(property in e) { 
-        logger(listSuspects[0][property]);
-        console.log(`${property}: ${listSuspects[0][property]}``);
-    }}
-);
-    
+listSuspects.forEach((e) => {
+  for (property in e) {
+    logger(listSuspects[0][property]);
+    console.log(`${property}: ${listSuspects[0][property]}`);
+  }
+});
+
+function oui() {
+  _.each(suspects, (name) => {
+    listSuspects.push(CreateSuspectObjects(name));
+  });
+}
