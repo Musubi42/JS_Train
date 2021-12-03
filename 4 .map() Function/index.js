09c8d2e3e30573f0387081024a890c5e7b58979a
Raphael THIBAUT <thibaut.raphael@protonmail.com>
2021-12-03 20:13:36 +0100

    map function

--- 4 .map() Function/index.js
+++ 4 .map() Function/index.js
@@ -1,13 +1,13 @@
 // Exercise 1
-const weapons = ['candlestick', 'lead pipe', 'revolver'];
+const weapons = ["candlestick", "lead pipe", "revolver"];
 
-const makeBroken = function(item) {
-    return `broken ${item}`;
-}
+const makeBroken = function (item) {
+  return `broken ${item}`;
+};
 
 // Wite the function .map
-const makeBrokenList = function(listItem) { 
-    return weapons.map(weapon => `broken ${weapon}`);
-    // const map1 = array1.map(x => x * 2);
-}
+const makeBrokenList = function (listItem) {
+  return weapons.map((weapon) => `broken ${weapon}`);
 
+  // const map1 = array1.map(x => x * 2);
+};
