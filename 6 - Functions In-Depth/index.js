c72c840a9cadf2f56e5db4241d00efa7197c9ef0
Raphael THIBAUT <thibaut.raphael@protonmail.com>
2021-12-19 12:12:21 +0100

    Avancement compréhension des fonctions

--- 6 - Functions In-Depth/index.js
+++ 6 - Functions In-Depth/index.js
@@ -84,4 +84,51 @@ const videoData = [
   },
 ];
 
-const nameSupects = videoData.filter((suspect) => videoData.name != "");
+const isSuspectsPresent = videoData.filter(
+  (suspect) => suspect.present === true
+);
+
+const suspectsNames = isSuspectsPresent.map((suspect) => suspect.name);
+
+const ezSuspects = videoData.map((suspect) => {
+  if (suspect.present === true) {
+    return suspect.name;
+  } else {
+    // Comment faire pour ne rien mettre dans ezSuspects quand la fonction arrive ici
+  }
+});
+
+// Can't use filter on the callback(?) (suspect)
+//const ezySuspects = videoData.map((suspect) => {
+//  suspect.filter((suspect) => suspect.present === true);
+//});
+
+//Comment faire une fonction self called
+// Tuple
+//(a = "It",b = "be",c = "could",d = "anyone") => ({
+//  console.log([
+//    [arguments[1], arguments[2]],
+//    [arguments[4], arguments[3]],
+//  ])
+//})();
+
+// Spread operator
+const createTupleSpread = (a, b, c, ...d) => {
+  return console.log([
+    [a, d],
+    [c, b],
+  ]);
+};
+
+createTupleSpread("It", "be", "could", "anyone", "to");
+
+// Faire un tour sur Mokka
+const errFunction = () => {
+  console.lag();
+};
+
+callFunction();
+
+const callFunction = () => {
+  console.log("J'suis une fonction");
+};
